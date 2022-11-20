4b74ad61dc2a03616473b388102435ebac2e0b70
Oğuzhan Baltacı <ogzhan.baltaci@gmail.com>
2022-11-20 23:29:07 +0300

    Course Finished

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -99,6 +99,7 @@ public class PlayerMovement : MonoBehaviour
             isAlive = false;
             myAnimator.SetTrigger("Dying");
             myRigidbody.velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
        }
     } 
 }
